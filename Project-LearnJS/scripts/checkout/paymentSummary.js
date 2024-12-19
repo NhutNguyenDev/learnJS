762ed7276b62a488243a3fd9f0ab0e4f3548b666
Nhut <devnhutnguyen@gmail.com>
2024-12-19 15:45:16 +0700

    update caculate payment when load checkout

---
+++ Project-LearnJS/scripts/checkout/paymentSummary.js
@@ -0,0 +1,51 @@
+import { cart } from "../../data/cart.js";
+import { products } from "../../data/products.js";
+export function renderPaymentSummary() {
+  let priceAllItem = 0;
+  let priceShipping = 0;
+
+  cart.forEach((product) => {
+    const idIProductInCart = product.productId;
+    const quanlityProduct = product.quanlity;
+
+    const productMatching = products.find(
+      (productInProducts) => productInProducts.id === idIProductInCart
+    );
+    priceAllItem += (productMatching.priceCents * product.quanlity) /100 ;
+  });
+
+  const paymentSummaryHTML = `
+          <div class="payment-summary-title">Order Summary</div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">$${priceAllItem}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">$${priceShipping}</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">$${priceAllItem + priceShipping}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">$${Math.round((priceAllItem + priceShipping)*0.1)}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">$${Math.round((priceAllItem + priceShipping)*0.1) + priceAllItem + priceShipping}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>`;
+
+    document.querySelector('.payment-summary').innerHTML = paymentSummaryHTML;
+
+}
